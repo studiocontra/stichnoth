f12691475d716b47a91968fe0feda7ecbc6691f4
Mateo Mateus <mateo.mateusm@gmail.com>
2023-07-13 22:21:13 -0500

    Hero

---
+++ scripts/sections/hero.js
@@ -0,0 +1,29 @@
+/**
+ * Rotating Features App
+ */
+const mainHeaderApp = Vue.createApp({
+	data() {
+		return {
+      currentLocale: '',
+		  isSearchOpen: false
+		}
+	},
+  mounted() {
+    new Swiper('.js-hero-slider', {
+      slidesPerView: 1,
+      mobileFirst: true,
+      effect: 'fade',
+      loop: true,
+      fadeEffect: {
+        crossFade: true
+      },
+      navigation: {
+        nextEl: '.js-hero-next',
+        prevEl: '.js-hero-prev',
+      },
+    });
+  }
+});
+
+mainHeaderApp.config.compilerOptions.delimiters = ['[[', ']]'];
+mainHeaderApp.mount('.hero');
--- theme/assets/section-header.js
+++
@@ -1,3 +0,0 @@
-"use strict";/**
- * Rotating Features App
- */var mainHeaderApp=Vue.createApp({data:function data(){return{currentLocale:"",isSearchOpen:!1}},methods:{changeLocale:function changeLocale(a,b){this.currentLocale=a,setTimeout(function(){document.getElementById(b).submit()},200)}}});mainHeaderApp.config.compilerOptions.delimiters=["[[","]]"],mainHeaderApp.mount(".header");
