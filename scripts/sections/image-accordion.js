49a12fe38a693626dfd79db0f456d8ebca9278d6
Mateo Mateus <mateo.mateusm@gmail.com>
2023-07-25 00:07:41 -0500

    Image Accordion

--- scripts/sections/image-accordion.js
+++ scripts/sections/image-accordion.js
@@ -5,14 +5,51 @@ const mainHeaderApp = Vue.createApp({
   data() {
     return {
       activeItem: 0,
+      totalItems: null,
+      timer: 5000,
+      myInterval: null
     };
   },
+  mounted() {
+    this.init();
+
+    this.myInterval = setInterval(() => {
+      this.nextItem();
+    }, this.timer);
+  },
   methods: {
-    openItem(idx, { target }) {
+    init() {
+      this.openItem(0);
+      this.totalItems = parseInt(this.$refs['total-input'].value);
+    },
+    resetInterval() {
+      clearInterval(this.myInterval);
+
+      this.myInterval = setInterval(() => {
+        this.nextItem();
+      }, this.timer);
+    },
+    openItem(idx) {
       this.activeItem = idx;
 
-      const contentHeight = target.nextElementSibling.scrollHeight;
-      console.log(contentHeight);
+      const contentWrapper = document.querySelector(`.accordion-item[data-idx="${idx}"] .accordion-item__content`);
+      const contentHeight = contentWrapper.scrollHeight;
+
+      contentWrapper.style.maxHeight = `${contentHeight}px`;
+      contentWrapper.querySelector('.progress-bar').style.width = '100%';
+    },
+    nextItem() {
+      this.activeItem = this.activeItem += 1;
+      if(this.activeItem > this.totalItems) this.activeItem = 0;
+
+      this.openItem(this.activeItem);
+    }
+  },
+  computed: {
+    styleObject() {
+      return {
+        transitionDuration: this.timer / 1000 + 's'
+      }
     }
   }
 });
