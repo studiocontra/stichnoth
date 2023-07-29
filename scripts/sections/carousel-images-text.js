69d826ef7873e96a9f023f7359b8dd50badcdf53
Mateo Mateus <mateo.mateusm@gmail.com>
2023-07-28 20:14:25 -0500

    Carousel images text

---
+++ scripts/sections/carousel-images-text.js
@@ -0,0 +1,22 @@
+/**
+ * Rotating Features App
+ */
+const mainHeaderApp = Vue.createApp({
+  mounted() {
+    new Swiper('.js-carousel-images-text-slider', {
+      loop: true,
+      speed: 1000,
+      slidesPerView: "auto",
+      spaceBetween: 0,
+      watchOverflow: true,
+      centerInsufficientSlides: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+      },
+    });
+  }
+});
+
+mainHeaderApp.config.compilerOptions.delimiters = ['[[', ']]'];
+mainHeaderApp.mount('.carousel-images-text');
