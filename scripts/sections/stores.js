5dbe17c6379ef1e22ad2ba876799a75f309235c7
Mateo Mateus <mateo.mateusm@gmail.com>
2023-07-31 20:57:00 -0500

    Stores

--- scripts/sections/stores.js
+++ scripts/sections/stores.js
@@ -5,17 +5,34 @@ const mainHeaderApp = Vue.createApp({
   data() {
     return {
       activeSection: null,
+      targetSectionTop: null,
     };
   },
+  mounted() {
+    window.addEventListener('scroll', () => this.activeOnScroll());
+  },
   methods: {
+    activeOnScroll() {
+      document.querySelectorAll('.js-stores-nav-item').forEach(item => {
+        const targetSection = document.querySelector(`.js-store-anchor[data-anchor="${item.dataset['section']}"]`);
+        const targetTop = targetSection.getBoundingClientRect().top + window.scrollY;
+
+        if(window.scrollY > targetTop) {
+          item.classList.add('active');
+        } else {
+          item.classList.remove('active');
+        }
+      })
+    },
     goToSection(target) {
-      const targetSection = document.querySelector(`.js-store-anchor[data-anchor="${target}"]`).getBoundingClientRect().top;
+      const targetSection = document.querySelector(`.js-store-anchor[data-anchor="${target}"]`).getBoundingClientRect().top + window.scrollY;
+      const storesNav = document.querySelector('.stores__nav').offsetHeight;
 
       this.activeSection = target;
-      console.log(target);
+      this.targetSectionTop = targetSection - storesNav;
 
       window.scrollTo({
-        top: targetSection,
+        top: this.targetSectionTop,
         behavior: 'smooth'
       })
     },
