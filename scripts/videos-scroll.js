5ddb180d51106cfd114477fdb81b6a603c89c473
Mateo Mateus <mateo.mateusm@gmail.com>
2023-09-13 20:44:10 -0500

    Hosted videos

--- scripts/videos-scroll.js
+++ scripts/videos-scroll.js
@@ -1,20 +1,30 @@
-var callback = function (entries) {
+let observerEnter = new IntersectionObserver(function (entries) {
   entries.forEach(({target, isIntersecting}) => {
-
     if(isIntersecting) {
-      target.querySelectorAll('.wrap-media').forEach(media => {
+      target.querySelectorAll('.wrap-media--video').forEach(media => {
         media.classList.add('is-playing');
       })
     }
   });
-};
+}, {
+  threshold: 0.35,
+});
 
-let observer = new IntersectionObserver(callback, {
-  threshold: 0.7,
+let observerLeave = new IntersectionObserver(function (entries) {
+  entries.forEach(({target, isIntersecting}) => {
+    if(!isIntersecting) {
+      target.querySelectorAll('.wrap-media--video').forEach(media => {
+        media.classList.remove('is-playing');
+      })
+    }
+  });
+}, {
+  threshold: 0,
 });
 
 var allItems = document.querySelectorAll(".js-scroll-video");
 
 allItems.forEach(section => {
-  observer.observe(section);
+  observerEnter.observe(section);
+  observerLeave.observe(section);
 })
